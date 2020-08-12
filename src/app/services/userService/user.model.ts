4616819e5630b5fc2d80a40d38a4749433b3ac45
adi_peled <adipeled499@gmail.com>
2020-08-12 12:58:58 +0300

    Auto-generated commit

--- src/app/services/userService/user.model.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    constructor(
-        public name: string = '',
-        public password: string = '',
-        public coins: number = 100,
-        public currency: string = ''
-    ) { }
-}
-
